a11dd474497cef39b6337f7c4b78ad9f955d6bee
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-15 06:41:05 -0500

    nouvelle modif

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ home: Scaffold(
 appBar: AppBar(
 title: Text('Atelier 2'),
 centerTitle: true,
-backgroundColor: Colors.blueAccent,
+backgroundColor: Colors.amberAccent,
 ),
 body: Container(
 alignment: Alignment.topCenter,
@@ -23,8 +23,37 @@ child: Column(
 children: [
 SizedBox(height: 20),
 Text(
-'Bienvenue dans cet atelier !',
-)
+'hello everybody ',style: TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.red,
+  ),
+),
+Image.network(
+  'https://picsum.photos/154',
+  width: 200,
+),
+Text(
+  'lets code it ',
+  textAlign: TextAlign.center,
+  style: TextStyle(fontSize: 16),
+),
+SizedBox(height: 30),
+Row(
+mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+children: [
+TextButton(onPressed: () {
+print("Tu as cliqué sur Commencer.");
+}, child: Text('Commencer')),
+OutlinedButton(
+onPressed: () {
+print("Tu as cliqué sur En savoir plus.");
+},
+child: Text('En savoir plus'),
+),
+],
+),
+
 ]         
 )
       
